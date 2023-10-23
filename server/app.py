38350881da53305fdcd2496e63128d6333dd662e
Rafael Moreira <rafa.siffert@gmail.com>
2023-10-23 18:36:57 -0400

    done

--- server/app.py
+++ server/app.py
@@ -3,3 +3,17 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# modify user() in server/app.py
+@app.route('/')
+def home():
+    return '<h1>Welcome to the home page</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+# append to server/app.py
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
